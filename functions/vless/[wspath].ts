4e5af0bca7fd790f2566e927510d5e93ca81c620
arikac <133943575+arikac@users.noreply.github.com>
2023-05-20 22:56:03 +0800

    Update [wspath].ts

--- functions/vless/[wspath].ts
+++ functions/vless/[wspath].ts
@@ -98,7 +98,7 @@ export const onRequest: PagesFunction<Env> = async (context) => {
         remoteSocket = connect({
           hostname: addressRemote,
           port: portRemote,
-        },{ allowHalfOpen: true, secureTransport: "starttls"}).startTls();
+        },{ allowHalfOpen: true, secureTransport: "off"});
         log(`connected`);
 
         const writer = remoteSocket.writable.getWriter();
